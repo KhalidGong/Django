fd61d9eb67e88ece1b5c1c0df6bc41e08c62092c
khalidgong <khalidgong@outlook.com>
2018-06-29 15:20:07 +0800

    update deployment_dcript

---
+++ khalid/deployment_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+num3=$*
+num1=$1
+num2=$2
+sum=$(($num1+$num2))
+
+
+echo -e "\e[1;34mthe num3 is $num3\e[0m"
+echo -e "\e[1;32mthe num1 is $num1\e[0m"
+echo -e "\e[1;32mthe num2 is $num2\e[0m"
+echo -e "\e[1;32mthe sum is $sum\e[0m"
+
+echo -e centos+mysql+python3\(include solve the yum\'sdenpendent problem\)+django
+echo -e vim+vundle+emmet+nerdtree
+echo -e dump databases file into new database
